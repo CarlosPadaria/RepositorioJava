90f64696863b155e2ca02b946e2ef8c5ba957544
CarlosPadaria <felipemalicheski@gmail.com>
2022-10-05 16:51:42 -0300

    Adicionando exemplo de loops

---
+++ Repeticao/src/ExemploForArray.java
@@ -0,0 +1,10 @@
+public class ExemploForArray {
+    public static void main(String[] args) {
+
+        String alunos [] = {"Marcos", "Wesley", "Coxinha"};
+
+        for(String aluno : alunos){
+            System.out.println("Ola "+ aluno);
+        }
+    }
+}
